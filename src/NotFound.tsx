a8b4a73591f3ec7b5d412a3a88a10200f07d8d63
Tyler <insertcleverusernamehere999@gmail.com>
2025-02-14 12:14:36 -0500

    styled error pages

--- src/NotFound.tsx
+++ src/NotFound.tsx
@@ -2,10 +2,17 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <div>
-      <h1>404 ERROR</h1>
-      <h1>The Page you were trying to look for does not exist!</h1>
-      <Link to="/">Go Home</Link>
+    <div className="not-found-page text-center p-2">
+      <h1 className="text-9xl text-emerald-900 underline">404 ERROR</h1>
+      <h1 className="text-6xl p-2">
+        The Page you were trying to look for does not exist!
+      </h1>
+      <Link
+        className="text-3xl text-green-600 underline hover:text-green-400 active:text-green-500"
+        to="/"
+      >
+        Go Home
+      </Link>
     </div>
   );
 };
--- src/budgets/BudgetErrorPage.tsx
+++ src/budgets/BudgetErrorPage.tsx
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 const BudgetErrorPage = () => {
   return (
-    <div className="budget-error-page">
-      <h1>401 ERROR</h1>
-      <h1>The budget you were trying to find either:</h1>
-      <ul>
-        <li>Does not exist</li>
-        <li>Does not belong to you</li>
-      </ul>
-      <Link to="/">Go Home</Link>
+    <div className="budget-error-page text-center p-2">
+      <h1 className="text-9xl text-emerald-900 underline">401 ERROR</h1>
+      <div className="error-list p-2">
+        <h1 className="text-5xl">The budget you were trying to find either:</h1>
+        <ul className="text-4xl">
+          <li>Does not exist</li>
+          <li>Does not belong to you</li>
+        </ul>
+      </div>
+
+      <Link
+        className="text-3xl text-green-600 underline hover:text-green-400 active:text-green-500"
+        to="/"
+      >
+        Go Home
+      </Link>
     </div>
   );
 };
