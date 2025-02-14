a5fcfb6543ef56b4622a027f0daf7250d2cae522
Tyler <insertcleverusernamehere999@gmail.com>
2025-02-14 12:43:40 -0500

    added navbar to dashboard

--- src/users/Dashboard.tsx
+++ src/users/Dashboard.tsx
@@ -1,5 +1,5 @@
 import { useState, useCallback } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useAppSelector, useAppDispatch } from "../features/hooks";
 import { logOutUser } from "../features/actions/auth";
 import EditUserForm from "./EditUserForm";
@@ -26,19 +26,36 @@ const Dashboard: React.FC = () => {
     dispatch(logOutUser({}));
     navigate("/");
   };
+
   return (
-    <div>
-      <div className="logout-button">
-        <button onClick={logOutAnNavigate}>Log Out</button>
+    <div className="dashboard-homepage">
+      <div className="buttons flex justify-around p-2 bg-emerald-900 sticky top-0">
+        <div className="logout-button">
+          <button
+            className="border border-blue-200 bg-blue-300 p-2 rounded-full hover:bg-blue-600 hover:text-white active:bg-blue-100 active:text-gray-900"
+            onClick={logOutAnNavigate}
+          >
+            Log Out
+          </button>
+        </div>
+        <div className="to-budgets-button">
+          <button
+            onClick={() => navigate("/budgets")}
+            className="border-2 border-green-600 bg-green-700 rounded-full p-2 flex justify-center hover:bg-green-300 active:bg-gray-100 active:text-green-700"
+          >
+            <TfiMoney className="text-lg my-1" />
+            <span>Check out your budgets </span>
+            <TfiMoney className="text-lg my-1" />
+          </button>
+        </div>
       </div>
       <div className="dashboard-usercard border-2 bg-white border-emerald-900 px-4 py-4 mx-4 my-4 shadow-xl">
         <div className="dashboard-username text-emerald-600">
           <h1 className="text-2xl text-center">{user?.username}</h1>
         </div>
-        <div className="dashboard-totalAssets text-emerald-600">
-          <p className="text-3xl text-center">
-            Assets Available: ${user?.totalAssets}
-          </p>
+        <div className="dashboard-totalAssets text-emerald-600 text-center">
+          <p className="text-xl">Assets Available:</p>
+          <p className="text-3xl">${user?.totalAssets}</p>
         </div>
 
         <div className="add-asset-button flex justify-center m-4">
@@ -51,19 +68,10 @@ const Dashboard: React.FC = () => {
         </div>
       </div>
       {showAssetForm && <EditUserForm hideForm={HideForm} />}
-      <div className="link-to-budgets m-5 flex justify-end">
-        <Link to="/budgets">
-          <button className="border-2 border-green-600 bg-green-700 rounded flex justify-center hover:bg-green-300 active:bg-gray-100 active:text-green-700">
-            <TfiMoney className="text-lg my-1" />{" "}
-            <span>Check out your budgets </span>
-            <TfiMoney className="text-lg my-1" />
-          </button>
-        </Link>
-      </div>
 
       <div className="recent-expenses-list">
         <h2 className="recent-expenses-list-title text-center text-3xl underline text-emerald-600 mb-2">
-          Recent Expenses
+          Your 10 Most Recent Expenses
         </h2>
         <ExpenseList
           expensesList={user!.expenses}
