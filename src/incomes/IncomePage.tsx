25808e37a30ccae7226f643358eea12f8e598be0
Tyler <insertcleverusernamehere999@gmail.com>
2025-04-30 14:05:45 -0400

    added custom hooks for income list page

--- src/incomes/IncomePage.tsx
+++ src/incomes/IncomePage.tsx
@@ -1,103 +1,22 @@
-import { useState, useCallback, useEffect } from "react";
-import { useParams, useNavigate, NavigateFunction } from "react-router-dom";
-import { setLoadError, setPageLoading } from "../features/slices/loadSlice";
-import { useAppDispatch } from "../features/hooks";
-import { AppDispatch } from "../features/store";
-import { Income } from "../interfaces/incomeInterfaces";
+import { useParams } from "react-router-dom";
 import ListHeader from "../ListHeader";
-import IncomeAPI from "../apis/IncomeAPI";
 import IncomeList from "./IncomeList";
 import NewIncomeForm from "./NewIncomeForm";
 import IncomePageButtons from "./IncomePageButtons";
-import { toast, Id } from "react-toastify";
+import useIncomePage from "./hooks/useIncomePage";
 
 // Shows the list of incomes the current user has
 const IncomePage = (): JSX.Element => {
   const { id } = useParams();
-  const dispatch: AppDispatch = useAppDispatch();
-  const navigate: NavigateFunction = useNavigate();
-  const notify = (): Id =>
-    toast.error("You have reached the maximum number of incomes");
-
-  // state that shows the form to add a new income
-  const [showIncomeForm, setShowIncomeForm] = useState<boolean>(false);
-  const [incomes, setIncomes] = useState<Income[]>([]);
-
-  // makes a request to retrive all of a single user's incomes and save them in component
-  // state on initial render
-  useEffect((): void => {
-    const getIncomes = async () => {
-      try {
-        dispatch(setPageLoading(true));
-        if (id) {
-          let newIncomes: Income[] = await IncomeAPI.getAllUserIncomes(id);
-          setIncomes(newIncomes);
-        }
-      } catch (err: any) {
-        dispatch(setLoadError(JSON.parse(err.message)));
-        navigate("/error");
-      } finally {
-        dispatch(setPageLoading(false));
-      }
-    };
-    getIncomes();
-  }, [id]);
-
-  // adds a single income to state after a form submission
-  const addToIncomeState = useCallback(
-    (income: Income): void => {
-      setIncomes((incomes) => [...incomes, income]);
-    },
-    [incomes]
-  );
-
-  // updates a single income in state after a form submission
-  const updateIncomeState = useCallback(
-    (income: Income): void => {
-      setIncomes((incomes) =>
-        incomes.map((i) => {
-          return i._id === income._id ? income : i;
-        })
-      );
-    },
-    [incomes]
-  );
-
-  // removes a single income from state after a button press
-  const removeFromIncomeState = useCallback(
-    (id: string): void => {
-      setIncomes((incomes) =>
-        incomes.filter((i) => {
-          return i._id !== id;
-        })
-      );
-    },
-    [incomes]
-  );
-
-  // updates state to show new income form unless the user already has 3 incomes
-  const showIncomeFormState = useCallback(
-    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
-      e.preventDefault();
-      if (incomes.length < 3) {
-        setShowIncomeForm(true);
-      } else {
-        notify();
-      }
-    },
-    [incomes, showIncomeForm]
-  );
-
-  // updates state to hide new income form
-  const hideIncomeFormState = useCallback(
-    (
-      e: React.MouseEvent<HTMLButtonElement, MouseEvent> | React.FormEvent
-    ): void => {
-      e.preventDefault();
-      setShowIncomeForm(false);
-    },
-    [showIncomeForm]
-  );
+  const {
+    showIncomeForm,
+    incomes,
+    addToIncomeState,
+    updateIncomeState,
+    removeFromIncomeState,
+    showIncomeFormState,
+    hideIncomeFormState,
+  } = useIncomePage(id);
 
   return (
     <div id="income-page">
---
+++ src/incomes/hooks/useIncomePage.ts
@@ -0,0 +1,98 @@
+import { useState, useEffect } from "react";
+import { useNavigate, NavigateFunction } from "react-router-dom";
+import { setLoadError, setPageLoading } from "../../features/slices/loadSlice";
+import { useAppDispatch } from "../../features/hooks";
+import { AppDispatch } from "../../features/store";
+import { Income } from "../../interfaces/incomeInterfaces";
+import IncomeAPI from "../../apis/IncomeAPI";
+import { toast, Id } from "react-toastify";
+
+type input = string | undefined;
+
+// custom hook for income page: includes retrieval of incomes, showing the form for a new income, adding new incomes
+// to the list, updating incomes in the list, and filtering out deleted incomes from the list
+const useIncomePage = (id: input) => {
+  const dispatch: AppDispatch = useAppDispatch();
+  const navigate: NavigateFunction = useNavigate();
+  const notify = (): Id =>
+    toast.error("You have reached the maximum number of incomes");
+
+  // state that shows the form to add a new income
+  const [showIncomeForm, setShowIncomeForm] = useState<boolean>(false);
+  const [incomes, setIncomes] = useState<Income[]>([]);
+
+  // makes a request to retrive all of a single user's incomes and save them in component
+  // state on initial render
+  useEffect((): void => {
+    const getIncomes = async () => {
+      try {
+        dispatch(setPageLoading(true));
+        if (id) {
+          let newIncomes: Income[] = await IncomeAPI.getAllUserIncomes(id);
+          setIncomes(newIncomes);
+        }
+      } catch (err: any) {
+        dispatch(setLoadError(JSON.parse(err.message)));
+        navigate("/error");
+      } finally {
+        dispatch(setPageLoading(false));
+      }
+    };
+    getIncomes();
+  }, [id]);
+
+  // adds a single income to state after a form submission
+  const addToIncomeState = (income: Income): void => {
+    setIncomes((incomes) => [...incomes, income]);
+  };
+
+  // updates a single income in state after a form submission
+  const updateIncomeState = (income: Income): void => {
+    setIncomes((incomes) =>
+      incomes.map((i) => {
+        return i._id === income._id ? income : i;
+      })
+    );
+  };
+
+  // removes a single income from state after a button press
+  const removeFromIncomeState = (id: string): void => {
+    setIncomes((incomes) =>
+      incomes.filter((i) => {
+        return i._id !== id;
+      })
+    );
+  };
+
+  // updates state to show new income form unless the user already has 3 incomes
+  const showIncomeFormState = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    e.preventDefault();
+    if (incomes.length < 3) {
+      setShowIncomeForm(true);
+    } else {
+      notify();
+    }
+  };
+
+  // updates state to hide new income form
+  const hideIncomeFormState = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | React.FormEvent
+  ): void => {
+    e.preventDefault();
+    setShowIncomeForm(false);
+  };
+
+  return {
+    showIncomeForm,
+    incomes,
+    addToIncomeState,
+    updateIncomeState,
+    removeFromIncomeState,
+    showIncomeFormState,
+    hideIncomeFormState,
+  };
+};
+
+export default useIncomePage;
